becb0cb75664342596f404cd52400547023bab8c
Gordon Anderson <gordon.b.anderson@gmail.com>
2017-11-19 21:57:12 +0700

    Added missing retired out

--- app/Models/DismissalMethod.php
+++ app/Models/DismissalMethod.php
@@ -25,31 +25,7 @@ class DismissalMethod extends Model
     const HIT_THE_BALL_TWICE=12;
     const TIMED_OUT=13;
     const NOT_OUT=14;
-
-
-
-    /*
-     * default=# select * from dismissal_methods;
- id |     created_at      |     updated_at      | short_name |       long_name       | slug
-----+---------------------+---------------------+------------+-----------------------+-------
-  1 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | c          | caught                | c
-  2 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | c&b        | caught and bowled     | c-b
-  3 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | b          | bowled                | b
-  4 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | st         | stumped               | st
-  5 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | htb        | handled the ball      | htb
-  6 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | otf        | obstructing the field | otf
-  7 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | dnb        | did not bat           | dnb
-  8 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | r/o        | run out               | r-o
-  9 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | r/h        | retired hurt          | r-h
- 10 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | r/o        | leg before wicket     | r-o-1
- 11 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | hw         | hit wicket            | hw
- 12 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | hbt        | hit the ball twice    | hbt
- 13 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | t/o        | timed out             | t-o
- 14 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | n/o        | not out               | n-o
- 15 | 2017-11-16 14:59:38 | 2017-11-16 14:59:38 | dnb        | did not bat           | dnb-1
-(15 rows)
-
-     */
+    const RETIRED_OUT=15;
 
     protected $table = 'dismissal_methods';
 
